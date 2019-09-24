c4cb31fdc0b6f34f64b72702f89b704c9341baf8
mostafaElsanadidy <50954337+mostafaElsanadidy@users.noreply.github.com>
2019-09-24 04:07:04 -0700

    first commit

---
+++ IBety_Project/IBety_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBety_Project
+//
+//  Created by mostafa elsanadidy on 9/24/19.
+//  Copyright © 2019 mostafa elsanadidy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
